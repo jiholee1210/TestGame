88d10fdb73e6a098502557f28f6bf617c46e7822
jiholee1210 <jiholee1210@gmail.com>
2024-06-14 15:31:26 +0900

    플레이어 피격 관련 및 기능 구현 / 플레이어 좌우 움직임 제한 조건 추가
    대쉬 중 피격 시 대쉬 중지, 무적 시간 종료 후 적 오브젝트와 겹쳐있을 시 충돌을 발생시키지 않고 바로 피격 이벤트 구현 필요함

--- First Project/Assets/PlayerMovement.cs
+++ First Project/Assets/PlayerMovement.cs
@@ -1,13 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
     // Start is called before the first frame update
     private Rigidbody2D rigidbody;
+    
     private bool isJumping = false;
-
     private int jumpCount = 0;
 
     private bool isDashing = false;
@@ -20,8 +21,13 @@ public class PlayerMovement : MonoBehaviour
     private bool canAttack = true;
     private float attackCooldown = 1f;
 
+    private bool isDamaged = false;
+    private float damagedTime = 0.2f;
+    private float invincibility = 0.7f;
+
     SpriteRenderer spriteRenderer;
     Animator animator;
+
     void Start()
     {
         rigidbody = GetComponent<Rigidbody2D>();   
@@ -50,8 +56,9 @@ public class PlayerMovement : MonoBehaviour
         StartCoroutine(Dash());
     }
 
+    // 플레이어 좌우 움직임
     void Move() {
-        if(!isDashing) {
+        if(!isDashing && !isDamaged) {
             float xInput = Input.GetAxisRaw("Horizontal");
 
             if(xInput != 0) {
@@ -67,6 +74,7 @@ public class PlayerMovement : MonoBehaviour
         }
     }
 
+    // 플레이어 점프
     void Jump() {
         if(isJumping) {
             Debug.Log("점프");
@@ -77,17 +85,17 @@ public class PlayerMovement : MonoBehaviour
             isJumping = false;
         }
     }
-
+    // 플레이어 공격
     IEnumerator Attack() {
         if(isAttacking) {
             canAttack = false;
-            
             animator.SetTrigger("Attack");
             isAttacking = false;
             yield return new WaitForSeconds(attackCooldown);
             canAttack = true;
         }
     }
+    // 플레이어 대쉬
     IEnumerator Dash() {
         if(isDashing) {
             canDash = false;
@@ -100,11 +108,36 @@ public class PlayerMovement : MonoBehaviour
             canDash = true;
         }
     }
+
+    // 플레이어 바닥 감지
     void OnTriggerEnter2D(Collider2D other) {
         Debug.Log("충돌 : " + other.tag);
         if(other.tag == "Platform") {
             jumpCount = 0;
             animator.SetBool("isJumping", false);
         }
+    }
+    
+    // 플레이어 피격 감지
+    void OnCollisionEnter2D(Collision2D other) {
+        if(other.gameObject.tag == "Enemy") {
+            Debug.Log("콜라이더 충돌" + other.gameObject.tag);
+            isDamaged = true;
+            StartCoroutine(onDamaged(other.transform.position));
+            
+        }    
+    }
+
+    // 플레이어 피격 애니메이션 및 기능
+    IEnumerator onDamaged(Vector2 targetPos) {
+        gameObject.layer = 8;
+        animator.SetTrigger("Hurt");
+        float dir = transform.position.x - targetPos.x >= 0 ? 1f : -1f;
+        Debug.Log(dir);
+        rigidbody.AddForce(new Vector2(dir, 0f) * 3f , ForceMode2D.Impulse);
+        yield return new WaitForSeconds(damagedTime);
+        isDamaged = false;
+        yield return new WaitForSeconds(invincibility);
+        gameObject.layer = 7;
     }
 }
