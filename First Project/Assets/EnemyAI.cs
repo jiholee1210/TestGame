452ee867affba62c591462703023a35b1eae138f
jiholee1210 <jiholee1210@gmail.com>
2024-06-25 20:49:15 +0900

    EnemyAI 적 피격 판정 추가 / 플레이어 바닥 감지 콜라이더 구분
    코루틴으로 피격 판정 추가 / HP 0일 시 오브젝트 삭제

--- First Project/Assets/EnemyAI.cs
+++ First Project/Assets/EnemyAI.cs
@@ -7,10 +7,15 @@ public class EnemyAI : MonoBehaviour
     private Rigidbody2D rigidbody;
     private SpriteRenderer spriteRenderer;
     private Animator animator;
+    public Transform playerPos;
 
     private int moveDir = 0;
     private int enemyFace = 0;
+
+    private float maxHp = 10;
+    private float Hp = 10;
     
+    private bool isDamaged = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -28,24 +33,49 @@ public class EnemyAI : MonoBehaviour
 
     void FixedUpdate() {
         Move();
+        Die();
     }
 
     void Move() { 
-        rigidbody.velocity = new Vector2(moveDir * 3f, rigidbody.velocity.y);
+        if (!isDamaged) {
+            rigidbody.velocity = new Vector2(moveDir * 3f, rigidbody.velocity.y);
+        }
     }
 
     void selectDir() {
-        moveDir = Random.Range(-1, 2);
-        
-        if(moveDir != 0) {
-            enemyFace = moveDir;
-            animator.SetBool("isWalking", true);
-        } else{
-            animator.SetBool("isWalking", false);
+        if(!isDamaged) {
+            moveDir = Random.Range(-1, 2);
+            
+            if(moveDir != 0) {
+                enemyFace = moveDir;
+                animator.SetBool("isWalking", true);
+            } else{
+                animator.SetBool("isWalking", false);
+            }
+
+            spriteRenderer.flipX = enemyFace > 0 ? false : true;
         }
-
-        spriteRenderer.flipX = enemyFace > 0 ? false : true;
-
         Invoke("selectDir", 1);
+    }
+
+    public IEnumerator TakeDamage(float damage) {
+        isDamaged = true;
+
+        Hp -= damage;
+        Debug.Log("적 체력 : " + Hp);
+
+        Vector2 damagedPos = playerPos.position.x > transform.position.x ? new Vector2(-3, 0) : new Vector2(3, 0);
+        Debug.Log(damagedPos);
+
+        rigidbody.velocity = Vector2.zero;
+        rigidbody.AddForce(damagedPos, ForceMode2D.Impulse);
+        yield return new WaitForSeconds(1.5f);
+        isDamaged = false;
+    }
+
+    void Die() {
+        if(Hp <= 0f) {
+            Destroy(gameObject);
+        }
     }
 }
